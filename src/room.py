5b5ca9b6f0802ef88eddbb622932deec88dd661c
adella <scratchglory@gmail.com>
2020-06-09 21:29:13 -0400

    added player and rooms

--- src/room.py
+++ src/room.py
@@ -1,2 +1,10 @@
 # Implement a class to hold room information. This should have name and
 # description attributes.
+
+class Room:
+    def __init__(self, desc1, desc2):
+        self.desc1 = desc1
+        self.desc2 = desc2
+
+    def __str__(self):
+        return f"{self.desc1}: {self.desc2}"
