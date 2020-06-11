222da499368eeaa8cac19d791d1268b14d06e695
adella <scratchglory@gmail.com>
2020-06-10 22:18:10 -0400

    changed the dictionary to vars

--- src/adv.py
+++ src/adv.py
@@ -3,29 +3,51 @@ from player import Player
 
 # Declare all the rooms
 
-room = {
-    'outside': Room("Outside Cave Entrance", "North of you, the cave mount beckons"),
+# room = {
+#     'outside': Room("Outside Cave Entrance", "North of you, the cave mount beckons"),
 
-    'foyer': Room("Foyer", """Dim light filters in from the south. Dusty passages run north and east."""),
+#     'foyer': Room("Foyer", """Dim light filters in from the south. Dusty passages run north and east."""),
 
-    'overlook': Room("Grand Overlook", """A steep cliff appears before you, falling into the darkness. Ahead to the north, a light flickers in the distance, but there is no way across the chasm."""),
+#     'overlook': Room("Grand Overlook", """A steep cliff appears before you, falling into the darkness. Ahead to the north, a light flickers in the distance, but there is no way across the chasm."""),
 
-    'narrow': Room("Narrow Passage", """The narrow passage bends here from west to north. The smell of gold permeates the air."""),
+#     'narrow': Room("Narrow Passage", """The narrow passage bends here from west to north. The smell of gold permeates the air."""),
 
-    'treasure': Room("Treasure Chamber", """You've found the long-lost treasure chamber! Sadly, it has already been completely emptied by earlier adventurers. The only exit is to the south."""),
-}
+#     'treasure': Room("Treasure Chamber", """You've found the long-lost treasure chamber! Sadly, it has already been completely emptied by earlier adventurers. The only exit is to the south."""),
+# }
+
+outside = Room("Outside Cave Entrance",
+               "North of you, the cave mount beckons"),
+
+foyer = Room(
+    "Foyer", """Dim light filters in from the south. Dusty passages run north and east."""),
+
+overlook = Room("Grand Overlook", """A steep cliff appears before you, falling into the darkness. Ahead to the north, a light flickers in the distance, but there is no way across the chasm."""),
+
+narrow = Room("Narrow Passage",
+              """The narrow passage bends here from west to north. The smell of gold permeates the air."""),
+
+treasure = Room("Treasure Chamber", """You've found the long-lost treasure chamber! Sadly, it has already been completely emptied by earlier adventurers. The only exit is to the south."""),
 
 
 # Link rooms together
 
-room['outside'].n_to = room['foyer']
-room['foyer'].s_to = room['outside']
-room['foyer'].n_to = room['overlook']
-room['foyer'].e_to = room['narrow']
-room['overlook'].s_to = room['foyer']
-room['narrow'].w_to = room['foyer']
-room['narrow'].n_to = room['treasure']
-room['treasure'].s_to = room['narrow']
+# room['outside'].n_to = room['foyer']
+# room['foyer'].s_to = room['outside']
+# room['foyer'].n_to = room['overlook']
+# room['foyer'].e_to = room['narrow']
+# room['overlook'].s_to = room['foyer']
+# room['narrow'].w_to = room['foyer']
+# room['narrow'].n_to = room['treasure']
+# room['treasure'].s_to = room['narrow']
+
+outside[0].n_to = foyer[0].name
+foyer[0].s_to = outside[0].name
+foyer[0].n_to = overlook[0].name
+foyer[0].e_to = narrow[0].name
+overlook[0].s_to = foyer[0].name
+narrow[0].w_to = foyer[0].name
+narrow[0].n_to = treasure[0].name
+treasure[0].s_to = narrow[0].name
 
 #
 # Main
@@ -38,11 +60,12 @@ room['treasure'].s_to = room['narrow']
 # Print an error message if the movement isn't allowed.
 #
 
-player = Player("One", "foyer")
+player = Player("One", outside)
 playing = True
 # * Prints the current room name
 # * Prints the current description (the textwrap module might be useful here).
-print("Current Room:", player.current_room)
+cr = player.current_room
+print("Current Room:", cr[0].name)
 
 while playing:
     # * Waits for user input and decides what to do.
@@ -53,28 +76,28 @@ while playing:
     if user_input == 'n':
         try:
             select = True
-            player.current_room = room[player.current_room].n_to.name
+            cr = cr[0].n_to
         except:
             print("Can't Go There!")
 
     elif user_input == 'e':
         try:
             select = True
-            player.current_room = room[player.current_room].e_to.name
+            cr = cr[0].e_to
         except:
             print("Can't Go There!")
 
     elif user_input == 's':
         try:
             select = True
-            player.current_room = room[player.current_room].s_to.name
+            cr = cr[0].s_to
         except:
             print("Can't Go There!")
 
     elif user_input == 'w':
         try:
             select = True
-            player.current_room = room[player.current_room].w_to.name
+            cr = cr[0].w_to
         except:
             print("Can't Go There!")
 
@@ -86,20 +109,17 @@ while playing:
         print("Try again, only with n, e, s, w... \n")
 
     if select == True:
-        if player.current_room == 'Outside Cave Entrance':
-            player.current_room = 'outside'
-        elif player.current_room == 'Foyer':
-            player.current_room = 'foyer'
-        elif player.current_room == 'Grand Overlook':
-            player.current_room = 'overlook'
-        elif player.current_room == 'Narrow Passage':
-            player.current_room = 'narrow'
-        elif player.current_room == 'Treasure Chamber':
-            player.current_room = 'treasure'
+        if cr == 'Outside Cave Entrance':
+            cr = outside
+        elif cr == 'Foyer':
+            cr = foyer
+        elif cr == 'Grand Overlook':
+            cr = overlook
+        elif cr == 'Narrow Passage':
+            cr = narrow
+        elif cr == 'Treasure Chamber':
+            cr = treasure
 
-        player.current_room = player.current_room.lower()
-
-        description = room[player.current_room].description
         print(
-            f"\n You are currently located {player.current_room}, \n {description} \n")
+            f"""You are currently located {cr[0].name}. \n Description: {cr[0].description} \n""")
         select = False
